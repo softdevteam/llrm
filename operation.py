3451095247af97688084ef75345a84e71c685236
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-06-17 15:31:30 +0100

    Add functions to get constants from llvm

---
+++ operation.py
@@ -0,0 +1,64 @@
+from llvm_wrapper import *
+from rpython.rtyper.lltypesystem import rffi, lltype
+
+class Operation(object):
+    ''' A class to represent an operation '''
+    not_set = True
+
+    def __init__(self, opcode, args = []):
+        self.opcode = opcode
+        self.args = args
+
+    def __eq__(self, other):
+        return self.ref == other.ref
+    def __eq__(self, other):
+        return isinstance(other, Operation) and self.opcode == other.opcode \
+                and self.args == other.args
+
+    def __str__(self):
+        return str(opcode)
+
+    # only works for integers
+    def _get_args(self, state):
+        ''' Returns a list of arguments represented as integers '''
+        arg_vals = []
+        for arg in self.args:
+            if LLVMIsConstant(arg):
+                arg_vals.append(LLVMConstIntGetSExtValue(arg))
+            else:
+                # XXX temporary dummy value for argc is 3
+                if Operation.not_set and "argc" in rffi.charp2str(LLVMPrintValueToString(arg)):
+                    state.set_variable(rffi.cast(rffi.INT, arg), 3)
+                    Operation.not_set = False
+                arg_vals.append(state.get_variable(rffi.cast(rffi.INT, arg)))
+        return arg_vals
+
+    class UnknownOpcodeException(Exception):
+        pass
+
+    def execute(self, state = None):
+        if self.opcode == LLVMRet:
+            return 0
+        elif self.opcode == LLVMAdd:
+            x, y = self._get_args(state)
+            return x + y
+        elif self.opcode == LLVMMul:
+            x, y = self._get_args(state)
+            return x * y
+        elif self.opcode == LLVMCall:
+            pass
+        elif self.opcode == LLVMAlloca:
+            pass
+        elif self.opcode == LLVMStore:
+            pass
+        elif self.opcode == LLVMOr:
+            x, y = self._get_args(state)
+            return x | y
+        elif self.opcode == LLVMLoad:
+            pass
+        elif self.opcode == LLVMShl:
+            x, y = self._get_args(state)
+            return x << y
+        else:
+            raise UnknownOpcodeException(opcode)
+        return 0
---
+++ state.py
@@ -0,0 +1,28 @@
+
+class State(object):
+    ''' Represents the state of a stack frame or of the entire program. '''
+
+    def __init__(self):
+        self.vars = []
+        self.var_offsets = {}
+
+    def get_variable(self, name):
+        ''' Returns the value of the specified local variable. '''
+        off = self.get_variable_off(name)
+        return self.vars[off]
+
+    #@jit.elidable_promote()
+    def get_variable_off(self, name):
+        ''' Returns the offset of the specified local variable. '''
+        off = self.var_offsets.get(name, -1)
+        if off == -1:
+            off = len(self.vars)
+            self.var_offsets[name] = off
+            self.vars.append(-1)
+        return off
+
+    def set_variable(self, name, val):
+        ''' Sets the given local variable to a specified value. '''
+        off = self.get_variable_off(name)
+        self.vars[off] = val
+
