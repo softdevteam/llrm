32c76569ba8eedaab8614c548b84bb190a9c2df2
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-06-20 22:05:10 +0100

    Move main loop in method run of Interpreter.

--- test.c
+++ test.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-int
-abs_test(int a, int b)
-{
-    if( a > b ) {
-        return a - b;
-    }
-    return b - a;
-}
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int a = 20;
     int b = 13;
+    int something = 100;
+    int c = a + b + something;
 
-    printf("%d\n", abs_test(a, b));
+    printf("c is %d and argc is %d and argc + 1 is %d\n", c, argc, argc +1);
+    printf("argc is %d\n", argc);
+    printf("5 * argc + 1 is %d\n", 5 * argc + 1);
 
     return 0;
 }
